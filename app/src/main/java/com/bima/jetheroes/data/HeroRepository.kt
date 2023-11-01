cea0a65156745ba4235892d70e140d02eaa3b570
bimamib <bimaprast1601@gmail.com>
2023-11-01 16:25:00 +0700

    menambahkan fungsi baru untuk mencari data pahlawan

--- app/src/main/java/com/bima/jetheroes/data/HeroRepository.kt
+++ app/src/main/java/com/bima/jetheroes/data/HeroRepository.kt
@@ -7,4 +7,10 @@ class HeroRepository {
     fun getHeroes(): List<Hero> {
         return HeroesData.heroes
     }
+
+    fun searchHeroes(query: String): List<Hero> {
+        return HeroesData.heroes.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
+    }
 }

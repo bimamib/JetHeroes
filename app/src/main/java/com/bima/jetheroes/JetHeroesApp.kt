b33142de34d5079d327a66e0494fd65bdfae31da
bimamib <bimaprast1601@gmail.com>
2023-11-01 13:53:41 +0700

    membuat item list yang menampilkan nama dan foto pahlawan

--- app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
@@ -1,17 +1,71 @@
 package com.bima.jetheroes
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.bima.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
-fun JetHeroesApp (
+fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
-    Box (modifier = modifier) {
+    Box(modifier = modifier) {
 
+    }
+}
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable { }
+    ) {
+        AsyncImage(
+            model = photoUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(
+            name = "H.O.S. Cokroaminoto",
+            photoUrl = ""
+        )
     }
 }
 

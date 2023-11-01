bddda805cde6c5ba660aa33b31e18abef0dd760a
bimamib <bimaprast1601@gmail.com>
2023-11-01 14:52:53 +0700

    membuat composable ScrollToTopButton

--- app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,6 +70,22 @@ fun HeroListItem(
                 .fillMaxWidth()
                 .weight(1f)
                 .padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    FilledIconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = stringResource(R.string.scroll_to_top),
         )
     }
 }

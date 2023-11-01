52977322585b4ea1f5f8f97686301567d096c5f3
bimamib <bimaprast1601@gmail.com>
2023-11-01 13:58:43 +0700

    menggunakan LazyColumn untuk menampilkan data

--- app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/bima/jetheroes/JetHeroesApp.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.bima.jetheroes.model.HeroesData
 import com.bima.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
@@ -24,7 +27,15 @@ fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier) {
-
+        LazyColumn {
+            items(HeroesData.heroes, key = { it.id }) { hero ->
+                HeroListItem(
+                    name = hero.name,
+                    photoUrl = hero.photoUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
 

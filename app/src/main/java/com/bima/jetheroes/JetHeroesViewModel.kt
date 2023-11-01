37d906f06a06f1d4855412ce96d5120546af3b0a
bimamib <bimaprast1601@gmail.com>
2023-11-01 16:14:06 +0700

    extend ke ViewModel, membuat parameter repository

--- app/src/main/java/com/bima/jetheroes/JetHeroesViewModel.kt
+++ app/src/main/java/com/bima/jetheroes/JetHeroesViewModel.kt
@@ -1,4 +1,16 @@
 package com.bima.jetheroes
 
-class JetHeroesViewModel {
+import androidx.lifecycle.ViewModel
+import com.bima.jetheroes.data.HeroRepository
+import com.bima.jetheroes.model.Hero
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
+    private val _groupedHeroes = MutableStateFlow(
+        repository.getHeroes()
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    )
+    val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
 }
